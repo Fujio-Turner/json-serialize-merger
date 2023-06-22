2c3d617d70d3a506255f738504c645eaa337f513
Fujio Turner <househippo@users.noreply.github.com>
2023-06-22 18:42:38 -0500

    More Lint fix Path to Json file

--- UnitTest/Test-BlindMerge.py
+++ UnitTest/Test-BlindMerge.py
@@ -1,20 +1,23 @@
 import json
 import sys
-sys.path.append('..')
-import scrap
+import os
+current_dir = os.path.dirname(os.path.abspath(__file__))
+parent_dir = os.path.dirname(current_dir)
+sys.path.insert(0, parent_dir)
+import JsonMerge
 
 if __name__ == "__main__":
 
-    with open('test-sample-json-data/OUPUT-New-doc-made.json', 'r') as file1:
+    with open(current_dir + '/test-sample-json-data/OUTPUT-New-doc-made.json', 'r') as file1:
         d = json.load(file1)
 
-    with open('test-sample-json-data/INPUT-Simple-doc-to-change.json', 'r') as file2:
+    with open(current_dir + '/test-sample-json-data/INPUT-Simple-doc-to-change.json', 'r') as file2:
         c = json.load(file2)
 
-    with open('test-sample-json-data/INPUT-Simple-doc.json', 'r') as file3:
+    with open(current_dir + '/test-sample-json-data/INPUT-Simple-doc.json', 'r') as file3:
         e = json.load(file3)
 
-    a = scrap.JSONMERGE()
+    a = JsonMerge.JSONMERGE()
 
     data = None
     try:
